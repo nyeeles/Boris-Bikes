ee8759b6c01e127233f66ae3e2bf1816a570a420
Nic Yeeles <n.yeeles@gmail.com>
2014-05-28 17:52:53 +0100

    Added Bike class - working with RSpec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,1 +1,23 @@
 require 'bike'
+
+describe 'Bike' do
+	
+	it 'should not be broken' do
+		bike = Bike.new
+		expect(bike).to_not be_broken
+	end
+
+	it 'should be able to break' do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
+	end
+
+	it 'should be able to get fixed' do
+		bike = Bike.new
+		bike.break!
+		bike.fix!
+		expect(bike).to_not be_broken
+	end
+
+end
